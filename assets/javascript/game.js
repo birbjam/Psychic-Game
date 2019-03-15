09e29042bacc6be4291d232416d70dbd909545d0
birbjam <41635757+birbjam@users.noreply.github.com>
2019-03-14 19:37:01 -0700

    Added basic js

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+
+var secretLetter = letters[Math.floor(Math.random() * letters.length)];
+
+console.log(secretLetter)
+
+
+
+document.onkeypress = function (event) {
+    var userGuess = event.key;
+
+    if (userGuess === secretLetter) {
+        wins++;
+    } else {
+        guesses--;
+    }
+
+    if (guesses === 0) {
+        losses++
+    }
+
+
+document.getElementById('wins').innerHTML = "Wins: " + wins;
+document.getElementById('losses').innerHTML = "Losses: " + losses;
+document.getElementById('guessesLeft').innerHTML = "Guesses left: " + guesses;
+
+}
