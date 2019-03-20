62de07e9a46fb6e058d2d8903cde2987a2bb82a5
birbjam <41635757+birbjam@users.noreply.github.com>
2019-03-19 21:08:43 -0700

    Fixed some bugs, actually works now

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,31 +4,41 @@ var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
 var wins = 0;
 var losses = 0;
 var guesses = 9;
-
 var secretLetter = letters[Math.floor(Math.random() * letters.length)];
-var userLetters = new Array();
+var wrongGuess = [];
 
-console.log(secretLetter)
 
+function reset() {
+    guesses = 9;
+    wrongGuess = [];
+    secretLetter = letters[Math.floor(Math.random() * letters.length)];
+    console.log(secretLetter);
+};
+
+console.log(secretLetter);
 
 
 document.onkeypress = function (event) {
     var userGuess = event.key;
 
     if (userGuess === secretLetter) {
+        reset();
         wins++;
+
     } else {
+        wrongGuess.push(userGuess);
         guesses--;
     }
 
     if (guesses === 0) {
         losses++
-    }
+        reset();
+    };
 
 
 document.getElementById('wins').innerHTML = "Wins: " + wins;
 document.getElementById('losses').innerHTML = "Losses: " + losses;
 document.getElementById('guessesLeft').innerHTML = "Guesses left: " + guesses;
-document.getElementById('guessesSoFar').innerHTML = "Your guesses so far: " + userGuess;
-
-}
+document.getElementById('guessesSoFar').innerHTML = "Your guesses so far: " + wrongGuess;
+    
+};
