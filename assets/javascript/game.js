c19e559f815e6df7c5b0e9b6705430b570b46f1e
birbjam <41635757+birbjam@users.noreply.github.com>
2019-10-29 20:47:41 -0700

    added styling and gh link

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,44 +1,39 @@
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
-    'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-var wins = 0;
-var losses = 0;
-var guesses = 9;
-var secretLetter = letters[Math.floor(Math.random() * letters.length)];
-var wrongGuess = [];
+var wins = 0
+var losses = 0
+var guesses = 10
+var secretLetter = letters[Math.floor(Math.random() * letters.length)]
+var wrongGuess = []
 
-
-function reset() {
-    guesses = 9;
-    wrongGuess = [];
-    secretLetter = letters[Math.floor(Math.random() * letters.length)];
-    console.log(secretLetter);
+function reset () {
+  guesses = 10
+  wrongGuess = []
+  secretLetter = letters[Math.floor(Math.random() * letters.length)]
+  console.log(secretLetter)
 };
 
-console.log(secretLetter);
-
+console.log(secretLetter)
 
 document.onkeypress = function (event) {
-    var userGuess = event.key;
+  var userGuess = event.key
 
-    if (userGuess === secretLetter) {
-        reset();
-        wins++;
+  if (userGuess === secretLetter) {
+    reset()
+    wins++
+  } else {
+    wrongGuess.push(userGuess)
+    guesses--
+  }
 
-    } else {
-        wrongGuess.push(userGuess);
-        guesses--;
-    }
+  if (guesses === 0) {
+    losses++
+    reset()
+  };
 
-    if (guesses === 0) {
-        losses++
-        reset();
-    };
-
-
-document.getElementById('wins').innerHTML = "Wins: " + wins;
-document.getElementById('losses').innerHTML = "Losses: " + losses;
-document.getElementById('guessesLeft').innerHTML = "Guesses left: " + guesses;
-document.getElementById('guessesSoFar').innerHTML = "Your guesses so far: " + wrongGuess;
-    
-};
+  document.getElementById('wins').innerHTML = 'Wins: ' + wins
+  document.getElementById('losses').innerHTML = 'Losses: ' + losses
+  document.getElementById('guessesLeft').innerHTML = 'Guesses left: ' + guesses
+  document.getElementById('guessesSoFar').innerHTML = 'Your guesses so far: ' + wrongGuess
+}
